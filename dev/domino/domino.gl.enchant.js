282f07ab95d2454d97f3512f3469c912f42a4710
Shuya Kanamaru <shellkana@gmail.com>
2013-05-28 19:25:29 +0900

    beta release

--- dev/domino/domino.gl.enchant.js
+++ dev/domino/domino.gl.enchant.js
@@ -11,10 +11,90 @@
 
 if (enchant.gl !== undefined) {( function() {
             enchant.gl.domino = {};
+            enchant.gl.domino.DominoStack = enchant.Class.create(enchant.gl.Sprite3D, {
+                initialize : function(distance) {
+                    enchant.gl.Sprite3D.call(this);
+                    if ( typeof distance === 'undefined') {
+                        this._distance = 0.75;
+                    } else {
+                        this._distance = distance;
+                    }
+                    this.lastDomino = null;
+                },
+                pushDomino : function(domino, distance) {
+                    if ( typeof distance === 'undefined') {
+                        distance = this._distance;
+                    }
+                    console.log(distance);
+                    this.addChild(domino);
+                    if (this.lastDomino !== null) {
+                        this.lastDomino.nextDomino = domino;
+                        this.lastDomino._distance = distance;
+                        domino.roll += this.lastDomino.roll;
+                        domino.x = this.lastDomino.x - Math.sin(-this.lastDomino.roll) * distance;
+                        domino.y = this.lastDomino.y - Math.cos(-this.lastDomino.roll) * distance;
+                    }
+                    this.lastDomino = domino;
+                }
+            });
             enchant.gl.domino.Domino = enchant.Class.create(enchant.gl.Sprite3D, {
                 initialize : function(width, height, depth) {
                     enchant.gl.Sprite3D.call(this);
                     this.mesh = enchant.gl.Mesh.createDomino(width, height, depth);
+                    this._roll = 0;
+                    this._pitch = 0;
+                    this._omega = 0;
+                    this._alpha = 0;
+                    this._distance = 0;
+                    this.nextDomino = null;
+                    this._isFalling = false;
+                    this.on("enterframe", function() {
+                        this._alpha = (this.pitch >= Math.PI / 2) ? 0 : 0.05 * Math.sin(this.pitch);
+                        if (this.pitch > Math.PI / 2) {
+                            this._omega = 0;
+                            this.pitch = Math.PI / 2;
+                        }
+                        this._omega += this._alpha;
+                        this.pitch += this._omega;
+                        if (this.nextDomino) {
+                            if (this.pitch > Math.asin(this._distance / this.mesh._height)) {
+                                if (!this.nextDomino._isFalling) {
+                                    this.nextDomino._omega = this._omega / 2 * 1.5;
+                                    this.nextDomino._isFalling = true;
+                                }
+                                this.pitch = this.calculatePitch(this.nextDomino.pitch)
+                            }
+                        }
+                    });
+                },
+                calculatePitch : function(nextPitch) {
+                    var answer = 0;
+                    var x = Math.PI / 2 - nextPitch;
+                    var A = Math.pow(this.mesh._height / Math.sin(x), 2);
+                    var B = this.mesh._height * this._distance;
+                    var C = this._distance * this._distance + this.mesh._height * this.mesh._height - A;
+                    if (x != 0) {
+                        answer = Math.acos((B + Math.sqrt(B * B - A * C)) / A);
+                    }
+                    return Math.PI / 2 - answer;
+                },
+                pitch : {
+                    set : function(rad) {
+                        this.rotatePitch(rad - this._pitch);
+                        this._pitch = rad;
+                    },
+                    get : function() {
+                        return this._pitch;
+                    }
+                },
+                roll : {
+                    set : function(rad) {
+                        this.rotateRoll(rad - this._roll);
+                        this._roll = rad;
+                    },
+                    get : function() {
+                        return this._roll;
+                    }
                 }
             });
             var proto = Object.getPrototypeOf(enchant.gl.Mesh);
@@ -23,17 +103,18 @@ if (enchant.gl !== undefined) {( function() {
                     sx = 1;
                 }
                 if ( typeof sy === 'undefined') {
-                    sy = 0.1;
+                    sy = 0.2;
                 }
                 if ( typeof sz === 'undefined') {
                     sz = 1.5;
                 }
                 var mesh = new enchant.gl.Mesh();
+                mesh._height = sz;
                 var vertices = [0.5, 0.5, 0.5, -0.5, 0.5, 0.5, -0.5, -0.5, 0.5, 0.5, -0.5, 0.5, 0.5, 0.5, -0.5, -0.5, 0.5, -0.5, -0.5, -0.5, -0.5, 0.5, -0.5, -0.5, 0.5, 0.5, 0.5, -0.5, 0.5, 0.5, -0.5, 0.5, -0.5, 0.5, 0.5, -0.5, 0.5, -0.5, 0.5, -0.5, -0.5, 0.5, -0.5, -0.5, -0.5, 0.5, -0.5, -0.5, 0.5, 0.5, 0.5, 0.5, -0.5, 0.5, 0.5, -0.5, -0.5, 0.5, 0.5, -0.5, -0.5, 0.5, 0.5, -0.5, -0.5, 0.5, -0.5, -0.5, -0.5, -0.5, 0.5, -0.5];
                 for (var i = 0, l = vertices.length; i < l; i += 3) {
                     vertices[i] *= sx;
                     vertices[i + 1] *= sy;
-                    vertices[i + 2] *= (vertices[i+2]>0)?sz*2:0;
+                    vertices[i + 2] *= (vertices[i + 2] > 0) ? sz * 2 : 0;
                 }
                 mesh.vertices = vertices;
 
--- dev/domino/main.js
+++ dev/domino/main.js
@@ -7,6 +7,7 @@ window.onload = function() {
         scene.getCamera().x = 20;
         scene.getCamera().y = 10;
         scene.getCamera().z = 2;
+        scene.getCamera().centerY=-3
         scene.getCamera().upVectorX = 0;
         scene.getCamera().upVectorY = 0;
         scene.getCamera().upVectorZ = 1;
@@ -22,9 +23,20 @@ window.onload = function() {
         sky.mesh.texture.diffuse = [0.0, 0.0, 0.0, 1.0];
         sky.mesh.texture.specular = [0.0, 0.0, 0.0, 1.0];
         scene.addChild(sky);
+        var dstack = new DominoStack();
+        scene.addChild(dstack);
         var d = new Domino();
-        scene.addChild(d);
-        
+        //d.roll = Math.PI / 4;
+        d.pitch = 0.01;
+        dstack.pushDomino(d);
+        var d3 = new Domino();
+        d3.roll = Math.PI / 6;
+        dstack.pushDomino(d3);
+        for (var i = 0; i < 10; i++) {
+            var d2 = new Domino();
+            d2.roll = Math.PI/24;
+            dstack.pushDomino(d2);
+        }
     };
     game.start();
 };
